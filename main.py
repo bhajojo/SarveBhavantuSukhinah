3a45a2af9c3247e962a72ae7c0ba95a2bf44bb2f
bhajojo <bharatof1984@gmail.com>
2020-10-03 14:25:30 +0530

    Added code to main

--- main.py
+++ main.py
@@ -91,6 +91,15 @@ def about():
     name= "Rugna Sahayata Kendra"
     return render_template("about.html", name=name)
 
+@app.route ('/templates/contact')
+def ContactsPage():
+    return render_template ("contact.html")
+
+@app.route ('/templates/about')
+def AboutPage():
+    return render_template ("about.html")
+
+
 @app.route('/templates/Registration', methods=['GET', 'POST'])
 def Registration():
 
